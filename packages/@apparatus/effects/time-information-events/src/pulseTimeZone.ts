807a598f523e7ffe2b223adf6783597dda7c1cd2
Fernando Via Canel <fernando.via@gmail.com>
2020-05-23 02:31:40 +0200

    🐞 apparatus/effects-time-information-events: fix android compatibility

--- packages/@apparatus/effects/time-information-events/src/pulseTimeZone.ts
+++ packages/@apparatus/effects/time-information-events/src/pulseTimeZone.ts
@@ -1,9 +1,10 @@
+import { getTimeZone } from 'react-native-localize'
 import { TCallbackTimeZone } from './types'
 
 let currentTimeZone: string
 
 export const pulseTimeZone = (callback: TCallbackTimeZone) => {
-  const nextTimeZone = Intl.DateTimeFormat().resolvedOptions().timeZone
+  const nextTimeZone = getTimeZone()
 
   if (currentTimeZone !== nextTimeZone) {
     callback(nextTimeZone)
